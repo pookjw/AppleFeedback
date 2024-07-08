01c107e69ed2e28110ad86dd254a85ce5dc3fdfd
Jinwoo Kim <kidjinwoo@me.com>
2024-07-08 21:21:01 +0900

    no message

---
+++ FB14235456/MyApp6/MyApp6App.swift
@@ -0,0 +1,17 @@
+//
+//  MyApp6App.swift
+//  MyApp6
+//
+//  Created by Jinwoo Kim on 7/8/24.
+//
+
+import SwiftUI
+
+@main
+struct MyApp6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
